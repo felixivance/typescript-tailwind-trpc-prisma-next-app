0c64ce754085cb768ffe0556c99432bf7ab9939c
Felix <felixrunye@gmail.com>
2023-07-05 11:42:38 +0300

    initial setup

---
+++ src/pages/_app.tsx
@@ -0,0 +1,9 @@
+import { type AppType } from "next/app";
+import { api } from "Y/utils/api";
+import "Y/styles/globals.css";
+
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default api.withTRPC(MyApp);
