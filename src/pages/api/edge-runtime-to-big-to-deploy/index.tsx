33ca496152634d76302c4582ab688f53d81b0904
Felix <felixrunye@gmail.com>
2023-08-16 21:06:38 +0300

    update

--- src/pages/api/edge-runtime-to-big-to-deploy/index.tsx
+++ src/pages/api/edge-runtime-to-big-to-deploy/index.tsx
@@ -8,8 +8,13 @@ import { NextApiRequest, NextApiResponse } from 'next';
 // export const runtime = 'experimental-edge'
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
     const data = await response.json();
 
     res.status(200).json(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
 }
